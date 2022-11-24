3acaa029b70e42739310ac79317692ff1c6ff43d
Javier Rojas <javier4le.dev@gmail.com>
2022-11-24 15:39:18 -0300

    ADD: Template and Authentication
    - Changed app name and database name
    - Added ui vue --auth from laravel/ui package
    - Added argon-dashboard template
    - Defined and created users table in migrations
    - Defined user model
    - Added form auth layout
    - Added login and register views

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            // $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
